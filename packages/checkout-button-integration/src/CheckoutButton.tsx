7523d4c25be7fd8c48e0f9c48514e4514b8f89f4
Sebastian Szafraniec <sebastian.szafraniec@bigcommerce.com>
2023-06-13 12:02:47 +0200

    chore(release): 1.325.3-prerelease.1686650460443 [skip ci]

--- packages/checkout-button-integration/src/CheckoutButton.tsx
+++ packages/checkout-button-integration/src/CheckoutButton.tsx
@@ -19,12 +19,28 @@ const CheckoutButton: FunctionComponent<CheckoutButtonProps> = ({
                 container: containerId,
                 onUnhandledError,
             },
-        }).catch(onUnhandledError);
-
+        })
+        .then(beautifyCheckoutButtons)
+        .catch(onUnhandledError);
+        
         return () => {
             deinitializeCustomer({ methodId }).catch(onUnhandledError);
         };
+        
     }, [containerId, deinitializeCustomer, initializeCustomer, methodId, onUnhandledError]);
+    
+    const beautifyCheckoutButtons = (): void => {
+        if (methodId === "amazonpay") {
+            if (!document.querySelector('.checkout-button-container')) {
+                return;
+            }
+
+            const container = document.querySelector('#amazonpayCheckoutButton > div') as unknown as HTMLElement;
+            if (!container) return;
+
+            (container?.shadowRoot?.querySelector('.amazonpay-button-view1') as unknown as HTMLElement).style.height = '36px'
+        }
+    }
 
     return <div id={containerId} />;
 };
