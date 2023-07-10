9bbf16186ce1b7450eaab4a2492840aa65fb9764
Sebastian Szafraniec <sebastian.szafraniec@bigcommerce.com>
2023-07-10 17:07:33 +0200

    feat(payment): PI-175 [Stripe] Guest Customers able to checkout without agreeing to Privacy Policy when using Stripe Link

--- packages/core/src/app/customer/StripeGuestForm.tsx
+++ packages/core/src/app/customer/StripeGuestForm.tsx
@@ -44,6 +44,7 @@ const StripeGuestForm: FunctionComponent<StripeGuestFormProps & FormikProps<Gues
     requiresMarketingConsent,
     privacyPolicyUrl,
     step,
+    status
 }) => {
 
     const [continueAsAGuestButton, setContinueAsAGuestButton] = useState(true);
@@ -54,7 +55,7 @@ const StripeGuestForm: FunctionComponent<StripeGuestFormProps & FormikProps<Gues
     const handleOnClickSubmitButton = () => {
         onContinueAsGuest({
             email: emailValue,
-            shouldSubscribe: false,
+            shouldSubscribe: !!status?.shouldSubscribe,
         });
     };
     const setEmailCallback = useCallback((authenticated: boolean, email: string) => {
@@ -69,10 +70,13 @@ const StripeGuestForm: FunctionComponent<StripeGuestFormProps & FormikProps<Gues
     }, [setContinueAsAGuestButton, onChangeEmail]);
 
     useEffect(() => {
+        if (status?.valSuccess) {
+            handleOnClickSubmitButton();
+        }
         if ((!step.isComplete || isNewAuth) && emailValue && authentication) {
             handleOnClickSubmitButton();
         }
-    }, [emailValue, authentication, isNewAuth]);
+    }, [emailValue, authentication, isNewAuth, status?.valSuccess]);
 
     const handleLoading = useCallback((mounted: boolean) => {
         setIsStripeLoading(mounted);
@@ -245,8 +249,11 @@ export default withLanguage(
                 shouldSubscribe: requiresMarketingConsent ? false : defaultShouldSubscribe,
                 privacyPolicy: false,
             }),
-            handleSubmit: (values, { props: { onContinueAsGuest } }) => {
-                onContinueAsGuest(values);
+            handleSubmit: ( values, { setStatus }) => {
+                setStatus({
+                    valSuccess: true,
+                    shouldSubscribe: values.shouldSubscribe,
+                  });
             },
             validationSchema: ({ language, privacyPolicyUrl }: StripeGuestFormProps & WithLanguageProps) => {
                 if (privacyPolicyUrl) {
